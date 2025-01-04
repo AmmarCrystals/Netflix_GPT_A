4d074dba847d690d529433162a6fdfbd26a4c236
AmmarCrystals <ammar80ansari@gmail.com>
2025-01-04 21:40:47 +1345

    Refactor Browse and LoginPage components; remove unused elements and integrate Header

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,7 +1,16 @@
+import Header from "./Header";
+
 const LoginPage = () => {
   return (
     <>
-      <h1>Hello From LoinPage</h1>
+      <div>
+        <Header/>
+      <img
+            className="w-full h-screen"
+            src="https://assets.nflxext.com/ffe/siteui/vlv3/2f5a878d-bbce-451b-836a-398227a34fbf/web/IN-en-20241230-TRIFECTA-perspective_5ab944a5-1a71-4f6d-b341-8699d0491edd_large.jpg"
+            alt=""
+          />
+      </div>
     </>
   );
 };
